7ad28a5ea35d7914eaa9bcf5bf69b5c2f40d3c45
shreyas.hingarh2@gmail.com <shreyas.hingarh2@gmail.com>
2023-01-07 16:31:14 -0800

    started

---
+++ HuffmanCode/HuffmanCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HuffmanCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
